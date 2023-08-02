f625e309b69b1081f38f4f508b2aa779d2f37d3f
Tom GERNEZ <tgernez@student.42.fr>
2023-08-02 15:11:34 +0200

    adding parsing

---
+++ srcs/error/error.cpp
@@ -0,0 +1,6 @@
+#include "IRC.hpp"
+
+void	error_str(const string error_message)
+{
+	cerr << "Error: " << error_message << endl;
+}
---
+++ srcs/init/init.cpp
@@ -0,0 +1,8 @@
+#include "IRC.hpp"
+
+bool	init(int &port, std::string &password)
+{
+	(void) port;
+	(void) password;
+	return true;
+}
